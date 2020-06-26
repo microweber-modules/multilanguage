d11118a74033fdfbe106ff0b1630efb2f8a8bd4c
Peter Ivanov <peter@microweber.com>
2020-06-26 11:09:49 +0300

    Incrementing version number to 2.3

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $config['ui'] = true; //if set to true, module will be visible in the toolbar
 $config['ui_admin'] = true; //if set to true, module will be visible in the admin panel
 $config['categories'] = "content";
 $config['position'] = 99;
-$config['version'] = 2.2;
+$config['version'] = 2.3;
 
 $config['tables'] = array(
     'multilanguage_translations' => array(
