b24c58503f0e46b102c9d0ecdcdcde6e04eac9ed
Bobi <bobi@microweber.bg>
2021-01-25 14:41:18 +0200

    update

--- event_binds_general.php
+++ event_binds_general.php
@@ -16,3 +16,7 @@ if (MW_VERSION >= '1.2.0') {
 event_bind('live_edit_toolbar_action_buttons', function () {
     echo '<module type="multilanguage/change_language"></module>';
 });
+
+event_bind('admin_mail_templates_message', function () {
+    echo '<module type="multilanguage" template="admin_module_reload" reload_module="admin/mail_templates/edit" class="float-right"></module>';
+});
