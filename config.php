d1ce64a6830813beb6b5e18a9e834eefa3366660
Bozhidar Slaveykov <50577633+bobimicroweber@users.noreply.github.com>
2021-04-01 13:34:45 +0300

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,7 @@ $config['ui'] = true; //if set to true, module will be visible in the toolbar
 $config['ui_admin'] = true; //if set to true, module will be visible in the admin panel
 $config['categories'] = "content";
 $config['position'] = 99;
-$config['version'] = 3.5;
+$config['version'] = 3.6;
 $config['require'] = [
     'microweber/microweber'=>'1.2'    
 ];
